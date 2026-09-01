06f8ff10194308e353787b1b746bec7caf2444e7
HardStyle777 <daiki.19810408.chiba@gmail.com>
2026-09-01 20:41:17 +0900

    追従カメラと敵モーションと攻撃間隔を改善

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tasogare-v10';
+const CACHE='tasogare-v11';
 const FILES=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./assets/ashwind-field.webp','./assets/hero-sword.webp','./assets/hero-warrior.webp','./assets/ashfang.webp','./assets/ashfang-walk32-v1.webp','./assets/ashfang-attack32-v1.webp','./assets/sword-walk32-v1.webp','./assets/sword-attack32-v1.webp','./assets/warrior-walk32-v1.webp','./assets/warrior-attack32-v1.webp','./assets/sword-anim-v3.webp','./assets/warrior-anim-v3.webp'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
