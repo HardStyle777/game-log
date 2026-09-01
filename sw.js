950401a4c4d5c896b42ae56b15167a61b4f31f67
HardStyle777 <daiki.19810408.chiba@gmail.com>
2026-09-01 19:44:31 +0900

    回転時の表示固定とキャラ画像待機を修正

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tasogare-v8';
+const CACHE='tasogare-v9';
 const FILES=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./assets/ashwind-field.webp','./assets/hero-sword.webp','./assets/hero-warrior.webp','./assets/ashfang.webp','./assets/ashfang-walk16-v1.webp','./assets/sword-walk16-v1.webp','./assets/warrior-walk16-v1.webp','./assets/sword-anim-v3.webp','./assets/warrior-anim-v3.webp'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
