821d18b196a12f8503a9cb72c6d4b5da209f1af1
HardStyle777 <daiki.19810408.chiba@gmail.com>
2026-09-07 20:50:15 +0900

    主人公スプライトと接写カメラを導入

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tasogare-v28';
+const CACHE='tasogare-v29';
 const FILES=['./','./index.html','./style.css','./game.js','./game.js?v=28','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
